f79ce6ef49df691e1dca606f6c836050f9203294
Patrick Sean Junghenn <junghenp@itserv.scss.tcd.ie>
2019-10-09 14:19:55 +0100

    Fixed tupla error, finished game prototype

--- NameGame.py
+++ NameGame.py
@@ -1,34 +1,50 @@
 import random
 import time
 
-names = ("Jean_Luc", "Patrick Bateman", "Elidia ","Rob T", "Tyler Scotch", "Benjamin Bauer", "Satoshi Nakamoto")
-facts = ("Boxing", "Mergers and Acquisitions ", "Biomedical", None, None, None, None, None, None, None, None, None, None)
+names = ["Jean_Luc", "Patrick Bateman", "Elidia ","Rob T", "Tyler Scotch", "Benjamin Bauer", "Satoshi Nakamoto"]
+facts = ["Boxing", "Mergers and Acquisitions", "Biomedical", None, None, None, None]
 points = 0
 
 
-hobbiesData = ("basketball", "returninng Video Tapes", "football", " poetry", "drama", "history")
+hobbiesData = ["basketball", "returninng Video Tapes", "football", "poetry", "drama", "history"]
 
-
-rand1 = random.randint(1,hobbiesData.__len__())
 
 for i in range(len(names)):
     if(facts[i] == None):
-        rand1 = random.randint(1, hobbiesData.__len__())
-
-        if (hobbiesData[rand1] == None):
-            #free give-away points
-            facts[i] = names[i]
-        else:
-            print(facts[i])
-            facts[i] = hobbiesData[rand1]
+        
+        rand1 = random.randint(0, hobbiesData.__len__()-1)
+        while(hobbiesData[rand1] == None):
+            rand1 = random.randint(0, hobbiesData.__len__()-1)
+        
+        facts[i] = hobbiesData[rand1]
         hobbiesData[rand1] = None
 
     print(names[i], "associated with", facts[i])
-    time.sleep(3)
+    time.sleep(2)
+    
+time.sleep(5)
 
-for x in range(20):
+for x in range(200):
     print(".")
 
+x = [0,1,2,3,4,5,6]
 
-
-
+for i in range(len(facts)):
+    
+    rand1 = random.randint(0, x.__len__()-1)
+    while(x[rand1] == None):
+        rand1 = random.randint(0, hobbiesData.__len__()-1)
+    
+    x[rand1] = None
+    
+    print("Who is associated with ",facts[rand1],"?",  sep='')
+    
+    inp = input("Enter the name: ") 
+    
+    if(str.upper(inp) == str.upper(names[rand1])):
+        points = points + 1
+        print("correct")
+    else:
+        print("kys", " it was ", names[rand1], sep = "")
+        
+print("you only got ", points, " points, haha")
